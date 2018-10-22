697366913cca8e92cb209ad61ad385f05fc3b7be
yanen <leryanen5@gmail.com>
2018-10-22 19:57:10 +0800

    Initial Commit

---
+++ innofest2018/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  innofest2018
+//
+//  Created by Ler Yan En on 22/10/18.
+//  Copyright © 2018 Ler Yan En. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ innofest2018Tests/innofest2018Tests.swift
@@ -0,0 +1,34 @@
+//
+//  innofest2018Tests.swift
+//  innofest2018Tests
+//
+//  Created by Ler Yan En on 22/10/18.
+//  Copyright © 2018 Ler Yan En. All rights reserved.
+//
+
+import XCTest
+@testable import innofest2018
+
+class innofest2018Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
