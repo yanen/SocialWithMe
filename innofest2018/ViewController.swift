de11b004bb848d732c991f1075cb45c73debae9d
yanen <leryanen5@gmail.com>
2018-10-23 11:46:38 +0800

    yes

--- innofest2018/ViewController.swift
+++ innofest2018/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var imageview: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Bonding"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func right(_ sender: Any) {
+        
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
 }
 
