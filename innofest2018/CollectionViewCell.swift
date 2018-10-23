d5dd0bb869e742eca888e53ea80f8fdbe4fd8781
yanen <leryanen5@gmail.com>
2018-10-23 11:22:54 +0800

    test

---
+++ innofest2018/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  innofest2018
+//
+//  Created by Ler Yan En on 23/10/18.
+//  Copyright © 2018 Ler Yan En. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet var imageview: UIImageView!
+    
+}
